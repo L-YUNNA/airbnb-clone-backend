197e88917d19bfd050ee481a55481e97b8115a51
L-YUNNA <imyun0315@gmail.com>
2023-05-07 15:32:43 +0900

    django_templates system

---
+++ rooms/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.see_all_rooms),  # path의 첫번째 인자는 이미 rooms안의 urls.py이므로 "rooms/"라고 적을 필요없어서 ""임
+    path("<int:room_pk>", views.see_one_room),
+]
