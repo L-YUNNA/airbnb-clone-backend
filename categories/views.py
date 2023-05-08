4b496429b38d0e3c288880bc488cce9f39d8cb0e
L-YUNNA <imyun0315@gmail.com>
2023-05-08 13:47:23 +0900

    apply APIView

--- categories/views.py
+++ categories/views.py
@@ -5,18 +5,20 @@ from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
+
 from .models import Category
 from .serializers import CategorySerializer
 
 # Create your views here.
-@api_view(["GET", "POST"])
-def categories(request):         # urls.py에서 호출된 함수에는 request 객체가 모두 주어짐
-    if request.method == "GET":
+class Categories(APIView):
+
+    def get(self, request):   # class안의 모든 메서드는 self를 가져야 함
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data,)
     
-    elif request.method == "POST":
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)  # request.data는 /categories 페이지에서 form에 json 양식으로 적은 데이터
         if serializer.is_valid():
             new_category = serializer.save()
@@ -24,21 +26,21 @@ def categories(request):         # urls.py에서 호출된 함수에는 request 
         else:
             return Response(serializer.errors,)
 
+class CategoryDetail(APIView):
 
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):  # pk는 categories/urls.py의 <int:pk>로부터 옴
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound     # raise가 실행되면 그 뒤 모든 코드는 실행되지 않음
-
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
-        return Response(serializer.data,)
+    def get_object(self, pk):  # pk는 categories/urls.py의 <int:pk>로부터 옴
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound     # raise가 실행되면 그 뒤 모든 코드는 실행되지 않음
     
-    elif request.method == "PUT":
+    def get(self, request, pk):   # pk 받아야하는 것 유념!
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data,)
+
+    def put(self, request, pk):
         serializer = CategorySerializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             partial=True,
         )
@@ -47,9 +49,12 @@ def category(request, pk):  # pk는 categories/urls.py의 <int:pk>로부터 옴
             return Response(CategorySerializer(updated_category).data,)
         else:
             return Response(serializer.errors)
-    
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+
 
         
